16bd468aa0116a3471b99b22634eef380068e04e
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2019-12-09 08:28:29 +0100

    Day 8 (part 2)

--- day8/day8.py
+++ day8/day8.py
@@ -6,6 +6,15 @@ input_file = os.path.join(os.path.dirname(__file__), 'input.txt')
 def chunkstring(string: str, length: int) -> str:
     return (string[0+i:length+i] for i in range(0, len(string), length))
 
+def combine_layers(front: str, back: str) -> str:
+    combined = ''
+    for i in range(len(front)):
+        if front[i] == '2':
+            combined += back[i]
+        else:
+            combined += front[i]
+    return combined
+
 def part1():
     with open(input_file) as input:
         image_data = input.readline().rstrip()
@@ -18,4 +27,24 @@ def part1():
     result = layer.count('1') * layer.count('2')
     print(f'Part 1: {result}')
 
+def part2():
+    with open(input_file) as input:
+        image_data = input.readline().rstrip()
+    width = 25
+    height = 6
+    layers = list(chunkstring(image_data, width*height))
+
+    result = layers[0]
+    for i in range(1, len(layers)):
+        result = combine_layers(result, layers[i])
+    
+    result = result.replace('0', ' ')
+    result = result.replace('1', '#')
+
+    print('Part2:')
+    for row in chunkstring(result, width):
+        print(row)
+
+
 part1()
+part2()
