6660b92805f21325b3a2dd1d27a96f86e1d4bd78
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2019-12-10 11:59:20 +0100

    Day 10 (part 1, rewrite)

--- day10/day10.py
+++ day10/day10.py
@@ -1,105 +1,68 @@
 import os
 from dataclasses import dataclass
+from math import atan2
 from typing import List, Set, Tuple
-from math import gcd
 
-DEBUG = False
 
 
 @dataclass(frozen=True)
-class Asteroid:
+class Point:
     x: int
     y: int
 
-    def __repr__(self):
-        return f'({self.x},{self.y})'
+    def __repr__(self): return f'({self.x},{self.y})'
 
 
 class AsteroidMap:
-    def __init__(self, textinput: List[str]):
+    def __init__(self, input: List[str]):
         self.asteroids = set()
-        self.max_x = len(textinput[0])-1
-        self.max_y = len(textinput)
+        self.max_x = len(input[0]) - 1
+        self.max_y = len(input)
 
-        for y, line in enumerate(textinput):
-            for x, point in enumerate(line):
-                if point == '#':
-                    self.asteroids.add(Asteroid(x, y))
+        for y, line in enumerate(input):
+            for x, column in enumerate(line):
+                if column == '#':
+                    self.asteroids.add(Point(x, y))
+    
+    def count_visible_asteroids(self, viewing_point: Point) -> int:
+        other_asteroids = self.asteroids.copy()
+        other_asteroids.remove(viewing_point)
 
-    def max_visible_asteroids(self) -> Tuple[Asteroid, int]:
-        asteroid = None
-        max_count = 0
-        for a in self.asteroids:
-            count = self.count_visible_asteroids(a)
+        viewing_angles = set()
+        for asteroid in other_asteroids:
+            angle = atan2(asteroid.y - viewing_point.y, asteroid.x - viewing_point.x)
+            viewing_angles.add(angle)
+
+        return len(viewing_angles)
+
+    def find_best_location_for_ims(self) -> Tuple[int, Point]:
+        max_asteroid = None
+        max_count = -1
+        for asteroid in self.asteroids:
+            count = self.count_visible_asteroids(asteroid)
             if count > max_count:
-                asteroid = a
                 max_count = count
+                max_asteroid = asteroid
 
-            if DEBUG:
-                print(f'{a}: visible asteroids: {count}')
+        return max_count, max_asteroid
 
-        return (asteroid, max_count)
-
-    def count_visible_asteroids(self, viewing_point: Asteroid) -> int:
-        visible_asteroids = self.asteroids.copy()
-        visible_asteroids.remove(viewing_point)
-
-        # for each asteroid, find all asteroids obscured by it
-        invisible_points = set()
-        for a in visible_asteroids:
-            invisible_points |= self.find_blocked_points(viewing_point, a)
-
-        if DEBUG:
-            invisible_asteroids = invisible_points & visible_asteroids
-            invisible_str = ', '.join(map(str, invisible_asteroids))
-            print(f'{viewing_point}: invisible asteroids: {invisible_str}')
-
-        visible_asteroids -= invisible_points
-        return len(visible_asteroids)
-
-    """ Find all points within the grid that are blocked by second, as seen from first """
-    def find_blocked_points(self, first: Asteroid, second: Asteroid) -> Set[Asteroid]:
-        delta_x = second.x - first.x
-        delta_y = second.y - first.y
-
-        divisor = gcd(delta_x, delta_y)
-        
-        if delta_x != 0:
-            delta_x = delta_x // divisor
-        if delta_y != 0:
-            delta_y = delta_y // divisor
-
-        next_x = second.x + delta_x
-        next_y = second.y + delta_y
-        blocked = set()
-        while (next_x >= 0) and (next_y >= 0) and (next_x <= self.max_x) and (next_y <= self.max_y):
-            blocked.add(Asteroid(next_x, next_y))
-            next_x += delta_x
-            next_y += delta_y
-
-        if DEBUG and (len(blocked) > 0):
-            blocked_str = ', '.join(map(str, blocked))
-            print(f'{first} - {second}: blocked points {blocked_str}')
-
-        return blocked
-
-
-def part1(filename: str) -> Tuple[Asteroid, int]:
+def read_file(filename: str) -> List[str]:
     input_file = os.path.join(os.path.dirname(__file__), filename)
     with open(input_file) as input:
-        map = AsteroidMap(input.read().splitlines())
+        lines = input.read().splitlines()
+    return lines
 
-    if DEBUG:
-        print(f'Number of asteroids: {len(map.asteroids)}')
+def part1(filename: str) -> Tuple[int, Point]:
+    map = AsteroidMap(read_file(filename))
 
-    asteroid, max_count = map.max_visible_asteroids()
-    print(f'Part 1 ({filename}): Asteroid{asteroid} visible: {max_count}')
-    return (asteroid, max_count)
+    count, asteroid = map.find_best_location_for_ims()
+    print(f'{filename}: {count} visible from {asteroid}')
+    return count, asteroid
 
-
-assert part1('dummy0.txt') == (Asteroid(3,4), 8)
-assert part1('dummy1.txt') == (Asteroid(5,8), 33)
-assert part1('dummy2.txt') == (Asteroid(1,2), 35)
-assert part1('dummy3.txt') == (Asteroid(6,3), 41)
-assert part1('dummy4.txt') == (Asteroid(11,13), 210)
+assert part1('dummy0.txt') == (8, Point(3, 4))
+assert part1('dummy1.txt') == (33, Point(5, 8))
+assert part1('dummy2.txt') == (35, Point(1, 2))
+assert part1('dummy3.txt') == (41, Point(6, 3))
+assert part1('dummy4.txt') == (210, Point(11, 13))
 part1('input.txt')
+    
