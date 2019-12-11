196a41e06b31ee7463bc44ecd37dc346e94a31eb
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2019-12-11 10:32:17 +0100

    Day 11 (part 1)

---
+++ day11/day11.py
@@ -0,0 +1,156 @@
+from __future__ import annotations
+
+import asyncio
+import os
+from dataclasses import dataclass, field, replace
+from typing import List
+
+from intcode import IntcodeComputer, read_intlist
+
+input_file = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+WHITE = 1
+BLACK = 0
+
+MOVE_UP = 0
+MOVE_RIGHT = 1
+MOVE_DOWN = 2
+MOVE_LEFT = 3
+
+TURN_LEFT = 0
+TURN_RIGHT = 1
+
+
+@dataclass(frozen=True, order=True)
+class Point:
+    x: int
+    y: int
+
+    def __repr__(self): return f'({self.x},{self.y})'
+
+    def point_in_direction(self, direction: int) -> Point:
+        if direction == MOVE_UP:
+            return Point(self.x, self.y - 1)
+        elif direction == MOVE_RIGHT:
+            return Point(self.x + 1, self.y)
+        elif direction == MOVE_DOWN:
+            return Point(self.x, self.y + 1)
+        elif direction == MOVE_LEFT:
+            return Point(self.x - 1, self.y)
+
+
+class PaintingRobot:
+    def __init__(self, program: List[int]):
+        self.input = asyncio.Queue()
+        self.output = asyncio.Queue()
+        self.computer = IntcodeComputer(program, self.input, self.output)
+        self.position = Point(0, 0)
+        self.direction = MOVE_UP
+        self.is_running = False
+        self.tiles_painted = 0
+
+        self.top_left = Point(0, 0)
+        self.bottom_right = Point(0, 0)
+        self.grid = {}
+        self.paint_current_tile(BLACK)
+
+    async def execute(self):
+        self.is_running = True
+
+        tasks = [
+            asyncio.create_task(self.run_computer()),
+            asyncio.create_task(self.handle_inout())
+        ]
+        await asyncio.gather(*tasks)
+
+    async def run_computer(self):
+        await self.computer.execute()
+        self.is_running = False
+
+    async def handle_inout(self):
+        while self.is_running:
+            await self.input.put(self.get_current_tile())
+
+            color = await self.output.get()
+            self.paint_current_tile(color)
+
+            turn = await self.output.get()
+            self.move_in_direction(turn)
+
+    def get_current_tile(self) -> int:
+        if self.position in self.grid:
+            return self.grid[self.position]
+        else:
+            return BLACK
+
+    def paint_current_tile(self, color: int):
+        if not self.position in self.grid:
+            self.tiles_painted += 1
+
+        self.grid[self.position] = color
+
+    def move_in_direction(self, turn: int):
+        if turn == TURN_LEFT:
+            self.direction = (self.direction-1) % 4
+        elif turn == TURN_RIGHT:
+            self.direction = (self.direction+1) % 4
+
+        self.position = self.position.point_in_direction(self.direction)
+
+        if not self.position in self.grid:
+            if (self.position.x < self.top_left.x):
+                self.top_left = replace(self.top_left, x=self.position.x)
+            if (self.position.y < self.top_left.y):
+                self.top_left = replace(self.top_left, y=self.position.y)
+            if (self.position.x > self.bottom_right.x):
+                self.bottom_right = replace(
+                    self.bottom_right, x=self.position.x)
+            if (self.position.y > self.bottom_right.y):
+                self.bottom_right = replace(
+                    self.bottom_right, y=self.position.y)
+
+    def print_grid(self):
+        print(f'Top left: {self.top_left}, bottom right: {self.bottom_right}')
+
+        # TODO fix off by one error
+        for y in range(self.top_left.y, (self.bottom_right.y - self.top_left.y)+1):
+            for x in range(self.top_left.x, (self.bottom_right.x - self.top_left.x)+1):
+                if Point(x, y) == self.position:
+                    color = '@'
+                elif Point(x, y) in self.grid:
+                    color = '#' if self.grid[Point(x, y)] == 1 else '.'
+                else:
+                    color = ' '
+                print(color, end='')
+            print()
+
+def test1():
+    robot = PaintingRobot([])
+    robot.paint_current_tile(WHITE)
+    robot.move_in_direction(TURN_LEFT)
+    robot.paint_current_tile(BLACK)
+    robot.move_in_direction(TURN_LEFT)
+    robot.paint_current_tile(WHITE)
+    robot.move_in_direction(TURN_LEFT)
+    robot.paint_current_tile(WHITE)
+    robot.move_in_direction(TURN_LEFT)
+    robot.paint_current_tile(BLACK)
+    robot.move_in_direction(TURN_RIGHT)
+    robot.paint_current_tile(WHITE)
+    robot.move_in_direction(TURN_LEFT)
+    robot.paint_current_tile(WHITE)
+    robot.move_in_direction(TURN_LEFT)
+
+    robot.print_grid()
+    print(f'Test1: {robot.tiles_painted} tiles painted')
+
+async def part1():
+    program = read_intlist(input_file)
+    robot = PaintingRobot(program)
+    await robot.execute()
+    robot.print_grid()
+    print(f'Part1: {robot.tiles_painted} tiles painted')
+
+
+test1()
+asyncio.run(part1())
---
+++ day11/intcode.py
@@ -0,0 +1,237 @@
+import logging
+from asyncio import Queue
+from typing import List
+
+
+def read_intlist(input_file: str) -> List[int]:
+    with open(input_file) as input:
+        line = input.readline()
+        return [int(s) for s in line.split(',')]
+
+
+def reverse(string: str) -> str:
+    string = "".join(reversed(string))
+    return string
+
+
+class Opcode:
+    def __init__(self, instruction: int):
+        self.opcode = instruction % 100
+        self._parameter_mode = reverse(str(instruction // 100)) + '0000'
+
+    def parameter_mode(self, parameter: int) -> int:
+        """ mode 0 = position mode, mode 1 = immediate mode """
+        return int(self._parameter_mode[parameter])
+
+
+class State:
+    def __init__(self, program: List[int], input_queue: Queue, output_queue: Queue):
+        self._memory = program.copy()
+        self._input = input_queue
+        self._output = output_queue
+        self._instruction_pointer = 0
+        self._relative_base = 0
+
+    def set_instruction_pointer(self, instruction_pointer: int):
+        self._instruction_pointer = instruction_pointer
+
+    def update_relative_base(self, offset: int):
+        self._relative_base += offset
+
+    def opcode(self) -> Opcode:
+        if (self._instruction_pointer < 0):
+            return None
+        return Opcode(self._read(self._instruction_pointer))
+
+    async def read_input(self) -> int:
+        return await self._input.get()
+
+    async def write_output(self, value: int):
+        await self._output.put(value)
+
+    def read_parameter(self, index: int) -> int:
+        """ first parameter has index 0 """
+        opcode = self.opcode()
+        parameter_mode = opcode.parameter_mode(index)
+        if (parameter_mode == 0):
+            address = self._read(self._instruction_pointer + index + 1)
+            return self._read(address)
+        elif (parameter_mode == 1):
+            return self._read(self._instruction_pointer + index + 1)
+        elif (parameter_mode == 2):
+            address = self._read(self._instruction_pointer + index + 1)
+            return self._read(self._relative_base + address)
+        else:
+            raise ValueError(
+                f'Unsupported parameter_mode {parameter_mode} for reading')
+
+    def write_parameter(self, index: int, value: int):
+        """ reads address from parameter index, writes value to that position """
+        opcode = self.opcode()
+        parameter_mode = opcode.parameter_mode(index)
+        if (parameter_mode == 0):
+            address = self._read(self._instruction_pointer + index + 1)
+            self._write(address, value)
+        elif (parameter_mode == 2):
+            address = self._read(self._instruction_pointer + index + 1)
+            self._write(self._relative_base + address, value)
+        else:
+            raise ValueError(
+                f'Unsupported parameter_mode {parameter_mode} for writing')
+
+    def _read(self, address: int) -> int:
+        self._ensure_enough_memory(address)
+        return self._memory[address]
+
+    def _write(self, address: int, value: int):
+        self._ensure_enough_memory(address)
+        self._memory[address] = value
+
+    def _ensure_enough_memory(self, address: int):
+        needed = address - len(self._memory) + 1
+        if (needed > 0):
+            self._memory = self._memory + [0] * needed
+
+
+class Instruction:
+    def size(self) -> int:
+        pass
+
+    def next_instruction(self, instruction_pointer: int) -> int:
+        return instruction_pointer + self.size()
+
+    async def execute(self, state: State):
+        next_ip = self.next_instruction(state._instruction_pointer)
+        state.set_instruction_pointer(next_ip)
+
+
+class AddInstruction(Instruction):
+    def size(self) -> int: return 4
+
+    async def execute(self, state):
+        a = state.read_parameter(0)
+        b = state.read_parameter(1)
+        result = a + b
+
+        state.write_parameter(2, result)
+        await super().execute(state)
+
+
+class MultiplyInstruction(Instruction):
+    def size(self) -> int: return 4
+
+    async def execute(self, state):
+        a = state.read_parameter(0)
+        b = state.read_parameter(1)
+        result = a * b
+
+        state.write_parameter(2, result)
+        await super().execute(state)
+
+
+class InputInstruction(Instruction):
+    def size(self) -> int: return 2
+
+    async def execute(self, state):
+        value = await state.read_input()
+        logging.debug('Input %d', value)
+        state.write_parameter(0, value)
+        await super().execute(state)
+
+
+class OutputInstruction(Instruction):
+    def size(self) -> int: return 2
+
+    async def execute(self, state):
+        output = state.read_parameter(0)
+        await state.write_output(output)
+        logging.debug('Output %d', output)
+        await super().execute(state)
+
+
+class JumpIfTrueInstruction(Instruction):
+    def size(self) -> int: return 3
+
+    async def execute(self, state):
+        value = state.read_parameter(0)
+        address = state.read_parameter(1)
+
+        if (value != 0):
+            state.set_instruction_pointer(address)
+        else:
+            await super().execute(state)
+
+
+class JumpIfFalseInstruction(Instruction):
+    def size(self) -> int: return 3
+
+    async def execute(self, state):
+        value = state.read_parameter(0)
+        address = state.read_parameter(1)
+
+        if (value == 0):
+            state.set_instruction_pointer(address)
+        else:
+            await super().execute(state)
+
+
+class LessThanInstruction(Instruction):
+    def size(self) -> int: return 4
+
+    async def execute(self, state):
+        a = state.read_parameter(0)
+        b = state.read_parameter(1)
+        result = 1 if (a < b) else 0
+        state.write_parameter(2, result)
+        await super().execute(state)
+
+
+class EqualsInstruction(Instruction):
+    def size(self) -> int: return 4
+
+    async def execute(self, state):
+        a = state.read_parameter(0)
+        b = state.read_parameter(1)
+        result = 1 if (a == b) else 0
+        state.write_parameter(2, result)
+        await super().execute(state)
+
+
+class AdjustRelativeBaseInstruction(Instruction):
+    def size(self) -> int: return 2
+
+    async def execute(self, state):
+        a = state.read_parameter(0)
+        state.update_relative_base(a)
+        await super().execute(state)
+
+
+class HaltInstruction(Instruction):
+    # TODO: betere manier verzinnen om programma te beeindigen
+    def size(self) -> int: return -9000
+
+
+INSTRUCTION_MAP = {
+    1: AddInstruction(),
+    2: MultiplyInstruction(),
+    3: InputInstruction(),
+    4: OutputInstruction(),
+    5: JumpIfTrueInstruction(),
+    6: JumpIfFalseInstruction(),
+    7: LessThanInstruction(),
+    8: EqualsInstruction(),
+    9: AdjustRelativeBaseInstruction(),
+    99: HaltInstruction()
+}
+
+
+class IntcodeComputer:
+    def __init__(self, program: List[int], input_queue: Queue, output_queue: Queue):
+        self._state = State(program, input_queue, output_queue)
+
+    async def execute(self):
+        current_opcode = self._state.opcode()
+        while current_opcode is not None:
+            instruction = INSTRUCTION_MAP[current_opcode.opcode]
+            await instruction.execute(self._state)
+            current_opcode = self._state.opcode()
